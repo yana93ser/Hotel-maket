b2ca19c0624b513d37cedcebc7bb387d655455af
yana93ser <90180341+yana93ser@users.noreply.github.com>
2024-07-12 18:31:07 +0300

    add calendar with style (display: none)

--- main.js
+++ main.js
@@ -15,17 +15,17 @@ const updateCalendar = () => {
     const firstDayIndex = firstDay.getDay();
     const lastDayIndex = lastDay.getDay();
 
-    const monthYearString = currentDate.toLocaleString('default', { month: 'long', year: "numeric" });
+    const monthYearString = currentDate.toLocaleString('default', {month: 'long', year: 'numeric'});
     monthYearElement.textContent = monthYearString;
 
     let datesHTML = '';
 
-    for (let i = firstDayIndex, i > 0; i--) {
+    for (let i = firstDayIndex; i > 0; i--) {
         const prevDate = new Date(currentYear, currentMonth, 0 - i + 1);
         datesHTML += `<div class="date inactive">${prevDate.getDate()}</div>`;
     }
 
-    for (let = 1; i <= totalDays; i++) {
+    for (let i = 1; i <= totalDays; i++) {
         const date = new Date(currentYear, currentMonth, i);
         const activeClass = date.toDateString() === new Date().toDateString() ? 'active' : '';
         datesHTML += `<div class="date ${activeClass}">${i}</div>`;
